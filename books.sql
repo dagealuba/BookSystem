7fc0cae6bd644ac88ae5d46a97beca258e64dd75
yunC <1206253228@qq.com>
2018-12-16 13:38:02 +0800

    Add files via upload

---
+++ books.sql
@@ -0,0 +1,38 @@
+-- 创建触发器，分别实现借书和还书时自动更新图书信息的在册数量；
+drop trigger if exists `borrow`;
+DELIMITER $$
+USE `booksystem`$$
+create trigger `borrow`
+after insert on `booksystem`.`borrow`
+for each row
+begin 
+update `booksystem`.`books` set `now_num`=`now_num` -1
+where `bookId`=new.`bookId`;
+END$$
+
+DELIMITER ;
+
+drop trigger if exists `return`;
+DELIMITER $$
+USE `booksystem`$$
+create trigger `return`
+after insert on `booksystem`.`borrow`
+for each row
+begin 
+update `booksystem`.`books` set `now_num`=`now_num` +1
+where `bookId`=new.`bookId`;
+END$$
+
+DELIMITER ;
+
+-- 触发器关于finishTime  DML
+drop trigger if exists `finishTime_input`;
+DELIMITER $$
+USE `booksystem`$$
+create trigger `finishTime_input`
+before insert on `booksystem`.`borrow`
+for each row
+begin
+set new.`finishTime`=date_add(new.`startTime`, interval 1 MONTH);
+END$$
+
